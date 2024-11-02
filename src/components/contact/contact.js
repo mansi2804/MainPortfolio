38c274ec38c71b177e7abff82d0e7b2db41ed3db
Mansi Patil <50865213+mansi2804@users.noreply.github.com>
2024-11-02 02:01:03 -0500

    Update contact.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -106,37 +106,6 @@ const Contact = () => {
                         Email sent successfully!
                     </Alert>
                 </Snackbar>
-                <div className={`flex-[0.4] bg-black-100 px-4 md:px-8 py-6 md:py-8 rounded-2xl ` + styles.contact_body} style={{ transform: 'translateX(0%) translateY(0px) translateZ(0px)' }}>
-                    <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">Get in touch</p>
-                    <h2 className="text-white font-black lg:text-[4rem] text-[2.5rem]">Contact Me.</h2>
-                    <form ref={form} onSubmit={sendEmail} className="mt-4 lg:mt-12 flex flex-col">
-                        <label className="flex flex-col lg:text-[1.2rem] text-[1rem]">
-                            <span className="text-white font-bold lg:mb-4 mb-2">Your Name *</span>
-                            <input type="text" name="user_name"
-                                value={formData.user_name}
-                                onChange={handleChange}
-                                placeholder="What's your name?" className={`lg:py-4 lg:px-6 py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-bold lg:text-[1.2rem] text-[1rem] ` + styles.bg_tertiary} />
-                                {<span className={errors.user_name? ` visible text-[1rem] mt-[5px] text-error`:`my-[15px] invisible text-[1rem] color`}>{errors.user_name}</span>}
-                        </label>
-                        <label className="flex flex-col">
-                            <span className="text-white font-bold lg:mb-4 mb-2">Your email *</span>
-                            <input type="text" name="user_email"
-                                value={formData.user_email}
-                                onChange={handleChange}
-                                placeholder="What's your email address?" className={`lg:py-4 lg:px-6 py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-bold ` + styles.bg_tertiary} />
-                                {<span className={errors.user_email? ` visible text-[1rem]  mt-[5px] text-error`:`my-[15px] invisible text-[1rem] `}>{errors.user_email}</span>}
-                        </label>
-                        <label className="flex flex-col">
-                            <span className={`text-white font-bold lg:mb-4 mb-2`}>Your Message *</span>
-                            <textarea rows="7" name="message"
-                                value={formData.message}
-                                onChange={handleChange}
-                                placeholder="What you want to say?" className={`lg:py-4 lg:px-6 py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-bold ` + styles.bg_tertiary}></textarea>
-                            {<span className={errors.message? ` visible text-[1rem]  mt-[5px] text-error`:`my-[15px] invisible text-[1rem] `}>{errors.message}</span>}
-                        </label>
-                        <button type="submit" className={`py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary ` + styles.bg_tertiary}>Send</button>
-                    </form>
-                </div>
                 <div className={`flex-[0.4] bg-black-100 p-0 lg:p-8 rounded-2xl ` + styles.contact_body + ` ` + styles.contact_body_right} style={{ transform: 'translateX(0%) translateY(0px) translateZ(0px)' }}>
                     <div className={styles.profile_card}>
                         <div className={styles.profile_card__image}>
